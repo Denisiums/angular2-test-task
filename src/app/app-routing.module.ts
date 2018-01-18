297818629c9fc48ece58cd3b9879f33aa0bc7285
Denisiums <denisiums@gmail.com>
2018-01-18 23:07:09 +0300

    Member not a child of department now

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,10 +10,17 @@ const routes: Routes = [
 
   {path: '', redirectTo: 'departments', pathMatch: 'full'},
   {path: 'departments', component: CompanyComponent, pathMatch: 'full'},
-  {path: 'departments/:id', component: DepartmentComponent,
-    children: [
-      {path: 'member/:id', component: MemberComponent},
-    ]},
+  {path: 'departments/:id', component: DepartmentComponent, pathMatch: 'full'},
+  {path: 'departments/:departmentId/member/:memberId', component: MemberComponent, pathMatch: 'full'},
+  {path: 'departments/:departmentId/member', component: MemberComponent},
+  // {path: 'departments/:departmentId/add-member', component: MemberComponent},
+  // {path: 'departments/:departmentId/member/', component: MemberLayoutComponent,
+    // children: [
+    //   {path: ':id', component: MemberComponent},
+    //   {path: '', redirectTo: ':id', pathMath: 'full'}
+    //
+    // ]
+  // },
   {path: '**', component: PageNotFoundComponent}
 ];
 
