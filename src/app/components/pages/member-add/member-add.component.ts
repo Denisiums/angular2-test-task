f64aadc70ba4817b0c5379914ffe73413a577b0b
Denisiums <denisiums@gmail.com>
2018-01-21 17:51:27 +0300

    Compile new member

--- src/app/components/pages/member-add/member-add.component.ts
+++ src/app/components/pages/member-add/member-add.component.ts
@@ -16,13 +16,15 @@ interface IAddMemberPagePending {
   styleUrls: ['./member-add.component.scss']
 })
 export class MemberAddComponent implements OnInit {
-  private newMember: Member;
+  public newMember: Member;
   public memberForm: FormGroup;
+  public formError: string = '';
 
 
   constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    console.log('init form!');
     this.newMember = Member.fromFrontend({
       name: '',
       description: '',
@@ -37,6 +39,58 @@ export class MemberAddComponent implements OnInit {
       gender: [this.newMember.gender, [Validators.required, Validators.maxLength(1)]],
       job: [this.newMember.job, [Validators.required, Validators.maxLength(100)]],
     });
+  }
+
+  public get skillsValid(): boolean {
+    return !!(this.newMember && this.newMember.skillsValid);
+  }
+
+  public addSkill(skill: ISkill): void {
+    if (!skill) {
+      return;
+    }
+
+    this.newMember.addSkill(skill);
+  }
+
+  public changeSkill(skill: ISkill): void {
+    if (!skill) {
+      return;
+    }
+
+    this.newMember.setSkill(skill);
+  }
+
+  public removeSkill(skill: ISkill): void {
+    if (!skill) {
+      return;
+    }
+
+    this.newMember.removeSkill(skill);
+  }
+
+  public create(): void {
+    if (!this.newMember || !this.skillsValid || !this.memberForm.valid) {
+      return;
+    }
+
+    const member = this.compileMember();
+    console.log('member to save: ', member);
+
+    console.log('create');
+    // todo: make a request to api
+  }
+
+  private compileMember(): Member {
+    if (!this.newMember || !this.skillsValid || !this.memberForm.valid) {
+      return;
+    }
+
+    this.newMember.name = this.memberForm.get('name').value.trim();
+    this.newMember.description = this.memberForm.get('description').value.trim();
+    this.newMember.job = this.memberForm.get('job').value.trim();
+    this.newMember.gender = this.memberForm.get('gender').value;
+    return this.newMember;
   }
 
 }
